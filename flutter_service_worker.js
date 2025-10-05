25cc4ae418f20d6ad960165e7f5f1da14959f673
hubertmucyo <h.mucyo@alustudent.com>
2025-10-05 21:59:12 +0530

    Built web
    Branch: main
    Commit: 5c883d87f0b71fd0d96a8d52b888680a26d6f65a
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3c6fad48fe2145aca11c514d81bbf9cc
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "1b188b46ffac734d855945513eccb756",
+"flutter_bootstrap.js": "dda393ba82a9dfc9e869b1cd034547f5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
